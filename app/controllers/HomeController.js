19bb8ed375cc2ca978a7d99126270300cf09a9c6
Safeer Ahmad <safeerahmad2050@gmail.com>
2023-05-07 23:24:03 +0500

    Scavenger creation api created

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,3 +1,29 @@
+const Scavenger = require("../models/Scavenger");
+const bcrypt = require("bcryptjs");
+
 exports.homePage = (req, res, next) => {
-	res.render('home');
+  res.render("home");
+};
+
+exports.createScavenger = (req, res, next) => {
+  console.log("req.body");
+  console.log(req.body);
+
+  bcrypt
+    .hash(req.body.password, 12)
+    .then((hashedPassword) => {
+      const scavenger_hunt = new Scavenger({
+        title: req.body.title,
+        location: req.body.location,
+        password: hashedPassword,
+        category: req.body.category,
+      });
+      return scavenger_hunt.save();
+    })
+    .then((result) => {
+      return res
+        .status(200)
+        .send({ result, message: "Scavenger Hunt successfully created" });
+    })
+    .catch((err) => console.log(err));
 };
--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const HomeController = require('../app/controllers/HomeController');
-const AuthController = require('../app/controllers/AuthController');
+const HomeController = require("../app/controllers/HomeController");
+const AuthController = require("../app/controllers/AuthController");
 
-router.get('/', HomeController.homePage);
-router.get('/login', AuthController.loginPage);
-router.post('/login', AuthController.login);
-router.post('/logout', AuthController.logout);
-router.get('/sign-up', AuthController.signUpPage);
-router.post('/sign-up', AuthController.signUp);
-router.get('/forgot-password', AuthController.forgotPasswordPage);
-router.post('/forgot-password', AuthController.forgotPassword);
+router.get("/", HomeController.homePage);
+router.get("/login", AuthController.loginPage);
+router.post("/login", AuthController.login);
+router.post("/logout", AuthController.logout);
+router.get("/sign-up", AuthController.signUpPage);
+router.post("/sign-up", AuthController.signUp);
+router.get("/forgot-password", AuthController.forgotPasswordPage);
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/create-scavenger", HomeController.createScavenger);
 
 module.exports = router;
